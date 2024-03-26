4ba84b1baab522c5e8aa42db43090959d6a2ff03
liweiyang <502936010@qq.com>
2024-03-26 15:18:47 +0800

    修改项目配置信息

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { fileURLToPath, URL } from "node:url";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
@@ -10,4 +10,12 @@ export default defineConfig({
     "../../assets/iconfont/iconfont.css"
   ],
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "~": fileURLToPath(new URL("./", import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@data": fileURLToPath(new URL("./src/data", import.meta.url)),
+    }
+  }
+  
 })
